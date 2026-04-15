c3c73b8854851a483bce39583b812307302c6e7b
EnzoCros <enzocros@hotmail.com>
2026-04-14 22:40:38 -0300

    fix: configura db.js com env vars e SSL para Aiven

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Enzo2805$',
-    database: 'portal_aprendizado'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = connection;
